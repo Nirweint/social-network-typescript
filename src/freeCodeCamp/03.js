245b90b39acb30a27d0a48999bc658a284464550
Nirweint <nirweint_1@mail.ru>
2021-10-02 18:39:54 +0300

    03

---
+++ src/freeCodeCamp/03.js
@@ -0,0 +1,11 @@
+function booWho(bool) {
+ if (typeof(bool) == "boolean") {
+   return true;
+ } else {
+   return false;
+ }
+}
+
+booWho(null);
+
+// Check if a value is classified as a boolean primitive. Return true or false.
