3f49e9ec1538ad3a19ee15dd795c2f61122c522b
Nirweint <nirweint_1@mail.ru>
2021-10-04 18:08:19 +0300

    7 fcc oop

--- src/freeCodeCamp/01.js
+++ src/freeCodeCamp/01.js
@@ -48,3 +48,12 @@ let dog2 = {
 };
 console.log(dog.name)
 console.log(dog.numLegs)
+
+// 7
+let dog3 = {
+	name: "Spot",
+	numLegs: 4,
+	sayLegs: function() {return `This dog has ${dog.numLegs} legs.`}
+};
+
+dog.sayLegs();
