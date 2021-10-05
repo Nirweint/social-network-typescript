793ba42d68662f01f0581e6f269ab767484bd302
Nirweint <nirweint_1@mail.ru>
2021-10-05 17:50:14 +0300

    20 cw

--- src/codeWars/01-cw.js
+++ src/codeWars/01-cw.js
@@ -192,3 +192,16 @@ function numberToString(num) {
 numberToString(67)
 
 // 20
+function uefaEuro2016(teams, scores){
+	if (scores[0] === scores[1]) {
+		return `At match ${teams[0]} - ${teams[1]}, teams played draw.`
+	}
+	if (scores[0] > scores[1]) {
+		return `At match ${teams[0]} - ${teams[1]}, ${teams[0]} won!`
+	}
+	if (scores[0] < scores[1]) {
+		return `At match ${teams[0]} - ${teams[1]}, ${teams[1]} won!`
+	}
+}
+
+uefaEuro2016(['Germany', 'Ukraine'], [2, 0])
