0ab9c272550443ccaccbf24e5c9e4f75e59ef9e0
Nirweint <nirweint_1@mail.ru>
2021-10-15 14:12:28 +0300

    21 added dialogs component structure

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,6 +3,28 @@ import s from './Dialogs.module.css';
 
 export function Dialogs() {
     return (
-        <div>Dialogs component</div>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <h2>Dialogs:</h2>
+                <div className={s.dialog + " " + s.active}>
+                    Alex
+                </div>
+                <div className={s.dialog}>
+                    Sofa
+                </div>
+                <div className={s.dialog}>
+                    Oleg
+                </div>
+                <div className={s.dialog}>
+                    Pasha
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>hello my friend</div>
+                <div className={s.message}> how are you?</div>
+                <div className={s.message}>fuck you</div>
+                <div className={s.message}>this is so beautiful</div>
+            </div>
+        </div>
     );
 }
